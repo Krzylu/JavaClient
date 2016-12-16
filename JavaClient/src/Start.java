dfcc5a746c1be0147ce320f665036f560fb8cafd
krzylu <krzylu90@gmail.com>
2016-12-16 21:54:43 +0100

    16.12;21:54

--- JavaClient/src/Start.java
+++ JavaClient/src/Start.java
@@ -1,20 +1,42 @@
-import java.util.*;
+import java.io.*;
+import java.net.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
 
 public class Start
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException
 	{
-		System.out.println("Hello World!");
+		URL url;
+        InputStream is = null;
+        BufferedReader br;
+        String doc = "";
 
-		Scanner input = new Scanner(System.in);
+        try {
+            url = new URL("http://www.imagefap.com/categories.php");
+            is = url.openStream();  // throws an IOException
+            br = new BufferedReader(new InputStreamReader(is));
 
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
-	}
+            String line;
+            while ((line = br.readLine()) != null) {
+                doc += "\n" + line;
+            }
+        } catch (MalformedURLException mue) {
+            mue.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            try {
+                if (is != null) is.close();
+            } catch (IOException ioe) {
+                // nothing to see here
+            }
+        }
+        System.out.println(doc);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = factory.newDocumentBuilder();
+        Document html = docBuilder.parse(new InputSource(new StringReader(doc)));
+        html.getElementsByTagName("");
+    }
 }
